074b25897b63fd9a262df016cfd00128e1351a24
Robinline95 <robin.wipff@epitech.eu>
2024-05-19 16:13:44 +0200

    fix: main updated init game and init clock

--- brasier_en_duel.c
+++ brasier_en_duel.c
@@ -16,15 +16,16 @@ static void event_close(window_t *main)
     }
 }
 
-static void init_main(window_t *main)
+void init_main(window_t *main)
 {
     main->mode.width = 1920;
     main->mode.height = 1080;
     main->mode.bitsPerPixel = 32;
-    main->window = sfRenderWindow_create(main->mode, "Brasier En Duel",
-    sfClose | sfResize, NULL);
     main->menu = sfTrue;
     main->music_bool = sfFalse;
+    main->exit_game_menu = sfFalse;
+    main->pause_menu = sfFalse;
+    main->end_of_game = sfFalse;
     main->background = "extra/menu/menu.jpg";
     main->sound_select = "extra/sound/menu.ogg";
     main->font = "extra/font/font.ttf";
@@ -34,7 +35,7 @@ static void init_main(window_t *main)
     main->selection = sfFalse;
 }
 
-static void init_game(list_t *list)
+void init_game(list_t *list)
 {
     list->txtrs[0].rectangle = (sfIntRect){0, 0, 50, 50};
     list->txtrs[0].position = (sfVector2f){800, 600};
@@ -42,6 +43,18 @@ static void init_game(list_t *list)
     list->txtrs[1].rectangle = (sfIntRect){0, 0, 50, 50};
     list->txtrs[1].position = (sfVector2f){960, 540};
     list->txtrs[1].clock = sfClock_create();
+    list->txtrs[2].rectangle = (sfIntRect){0, 0, 166, 166};
+    list->txtrs[2].position = (sfVector2f){10, 10};
+    list->txtrs[2].clock = sfClock_create();
+    list->txtrs[3].rectangle = (sfIntRect){0, 0, 166, 166};
+    list->txtrs[3].position = (sfVector2f){93, 10};
+    list->txtrs[3].clock = sfClock_create();
+    list->txtrs[4].clock = sfClock_create();
+    list->txtrs[4].countdown = 31;
+    list->txtrs[5].clock = sfClock_create();
+    list->txtrs[5].countdown = 61;
+    list->txtrs[6].clock = sfClock_create();
+    list->txtrs[6].countdown = 91;
 }
 
 int main(int argc, char **argv)
@@ -51,10 +64,9 @@ int main(int argc, char **argv)
     button_t button;
     texture_t texture;
 
-    if (argc != 1)
-        return 84;
     init_main(&main);
     init_game(&list);
+    main.window = sfRenderWindow_create(main.mode, "BrasierEnDuel", 4 | 2, 0);
     while (sfRenderWindow_isOpen(main.window)) {
         event_close(&main);
         sfRenderWindow_clear(main.window, sfTransparent);
